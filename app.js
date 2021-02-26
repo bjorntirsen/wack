e61bffd5765f40f707753852628ee943704cb78d
Björn Tirsén <bjorntirsen@gmail.com>
2021-02-26 14:14:37 +0100

    Add EJS homepage

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
+const path = require('path');
 const app = require('express')();
 
+//Setting up EJS
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
 app.listen(3000, function () {
-    console.log('App running on port 3000');
+  console.log('App running on port 3000');
 });
