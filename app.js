2b2c8fe4d55fb0d1cfaf96731849d9f570b118c5
Björn Tirsén <bjorntirsen@gmail.com>
2021-03-09 10:17:11 +0100

    ♻️Breakout into routes

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
-//Core modules
 const path = require('path');
-//Third party modules
 const express = require('express');
 const app = express();
-const mongoose = require('mongoose');
 
-//Setup mongoose DB
+const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/wack', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -16,6 +13,9 @@ db.once('open', () => {
   console.log('Connected to MongoDB');
 });
 
+const indexRouter = require('./routes/index');
+const userRouter = require('./routes/user');
+
 const Channel = require('./models/channel');
 const Post = require('./models/post');
 
@@ -24,74 +24,15 @@ const http = require('http').Server(app);
 //Setting up Socket.IO with the server
 const io = require('socket.io')(http);
 
-//Setting up EJS
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-//Setting up access to public folder
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
-//Let express read html forms
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.render('index');
-});
-
-app.get('/home', (req, res) => {
-  Channel.find((err, data) => {
-    if (err) return console.error(err);
-    channels = data;
-    console.log(data);
-    res.render('home', { channels });
-  });
-});
-
-app.get('/channels/create', (req, res) => {
-  res.render('ch_create.ejs', { cssdir: '../' });
-});
-
-app.post('/channels/create', (req, res) => {
-  const channel = new Channel({
-    name: req.body.name,
-    description: req.body.description || '',
-    private: req.body.private ? true : false,
-  });
-  channel.save((err) => {
-    if (err) return console.error(err);
-    channel.created();
-    res.redirect('/home');
-  });
-});
-
-app.get('/channels/:id', async (req, res) => {
-  let channels = [];
-  await Channel.find((err, data) => {
-    if (err) return console.error(err);
-    channels = data;
-  });
-  let channel = {};
-  await Channel.findOne({ _id: req.params.id }, (err, data) => {
-    if (err) return console.error(err);
-    channel = data;
-  });
-  res.render('channel', { channel, channels, cssdir: '../' });
-});
-
-app.post('/channels/:id', (req, res) => {
-  const post = new Post({
-    by: req.body.user,
-    content: req.body.content,
-  });
-  Channel.updateOne(
-    { _id: req.params.id },
-    { $push: { posts: post } },
-    (err) => {
-      if (err) return console.error(err);
-      res.redirect(`/channels/${req.params.id}`);
-    }
-  );
-});
+app.use('/', indexRouter);
+app.use('/user', userRouter);
 
 io.on('connection', (socket) => {
   console.log('a user connected');
---
+++ routes/user.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const router = express.Router();
+
+router.use(express.urlencoded({ extended: true }));
+
+const Channel = require('../models/channel');
+
+router.get('/home', (req, res) => {
+  Channel.find((err, data) => {
+    if (err) return console.error(err);
+    res.render('home.ejs', { channels: data });
+  });
+});
+
+router.get('/channels/create', (req, res) => {
+  res.render('ch_create.ejs', { cssdir: '../' });
+});
+
+router.post('/channels/create', (req, res) => {
+  const channel = new Channel({
+    name: req.body.name,
+    description: req.body.description || '',
+    private: req.body.private ? true : false,
+  });
+  channel.save((err) => {
+    if (err) return console.error(err);
+    console.log('Channel created.');
+    res.redirect('/home');
+  });
+});
+
+router.get('/delete/:id', (req, res) => {
+  Channel.deleteOne({ _id: req.params.id }, (err, data) => {
+    if (err) return console.error(err);
+    console.log(req.params.id + 'deleted');
+    res.redirect('/');
+  });
+});
+
+router.get('/channels/:id', async (req, res) => {
+  let channels = [];
+  await Channel.find((err, data) => {
+    if (err) return console.error(err);
+    channels = data;
+  });
+  let channel = {};
+  await Channel.findOne({ _id: req.params.id }, (err, data) => {
+    if (err) return console.error(err);
+    channel = data;
+  });
+  res.render('channel', { channel, channels, cssdir: '../' });
+});
+
+router.post('/channels/:id', (req, res) => {
+  const post = new Post({
+    by: req.body.user,
+    content: req.body.content,
+  });
+  Channel.updateOne(
+    { _id: req.params.id },
+    { $push: { posts: post } },
+    (err) => {
+      if (err) return console.error(err);
+      res.redirect(`/channels/${req.params.id}`);
+    }
+  );
+});
+
+module.exports = router;
